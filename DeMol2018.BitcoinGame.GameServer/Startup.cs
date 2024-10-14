bbb4de0be7eef70abb2aa07eb10b2a2e20ba95cf
Rutger van Willigen <rutger@infi.nl>
2024-10-14 12:25:47 +0200

    Remove MVC from server

--- DeMol2018.BitcoinGame.GameServer/Startup.cs
+++ DeMol2018.BitcoinGame.GameServer/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace DeMol2018.BitcoinGame.GameServer
 {
@@ -18,19 +17,24 @@ namespace DeMol2018.BitcoinGame.GameServer
         {
             services.AddCors(options =>
             {
-                options.AddDefaultPolicy(
-                    builder =>
-                    {
-                        builder
-                            .AllowAnyHeader()
-                            .WithMethods("GET", "POST")
-                            .WithOrigins(Configuration["Cors:Origins"]!.Split(','))
-                            .AllowCredentials();
-                    });
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder
+                        .AllowAnyHeader()
+                        .WithMethods("GET", "POST")
+                        .WithOrigins(Configuration["Cors:Origins"]!.Split(','))
+                        .AllowCredentials();
+                });
             });
 
             var connectionString = Configuration.GetConnectionString("BitcoinGameDatabase");
-            services.AddDbContext<BitcoinGameDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddDbContext<BitcoinGameDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString, o =>
+                {
+                    o.UseQuerySplittingBehavior(QuerySplittingBehavior.SingleQuery);
+                });
+            });
 
             services.AddTransient<GameService>();
             services.AddTransient<PlayerService>();
@@ -38,42 +42,28 @@ namespace DeMol2018.BitcoinGame.GameServer
             services.AddTransient<WalletService>();
 
             services.AddSignalR();
-            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            PrepareDatabase(app);
+
             app.UseCors();
-
-            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
-            {
-                var context = serviceScope.ServiceProvider.GetService<BitcoinGameDbContext>();
-                context.Database.Migrate();
-            }
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
             app.UseRouting();
 
             app.UseEndpoints(routes =>
             {
                 routes.MapHub<BitcoinGameHub>("/bitcoinGameHub");
-
-                routes.MapControllerRoute(
-                    "default",
-                    "{controller=Home}/{action=Index}/{id?}");
-
-                routes.MapFallbackToController("Index", "Home");
             });
+        }
 
-            app.UseStaticFiles();
+        private void PrepareDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<BitcoinGameDbContext>();
+                context.Database.Migrate();
+            }
         }
     }
 }
