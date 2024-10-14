b69e99b6c285272e9fdb9dc712d0ec9554e5a7e6
Rutger van Willigen <rutger@infi.nl>
2024-10-14 11:25:22 +0200

    Cleanup webpack configs, remove server logic

--- DeMol2018.BitcoinGame.ReactApp/webpack.config.js
+++ DeMol2018.BitcoinGame.ReactApp/webpack.config.js
@@ -1,59 +1,59 @@
 const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { merge } = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (env) => {
     const isDevBuild = true;// !(env && env.prod);
+    const clientBundleOutputDir = './public/dist';
 
-    // Configuration in common to both client-side and server-side bundles
-    const sharedConfig = () => ({
+    const clientBundleConfig = {
         mode: isDevBuild ? "development" : "production",
-        stats: { modules: false },
-        resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-        output: {
-            filename: '[name].js',
-            path: '/dist' // Webpack dev middleware, if enabled, handles requests for this URL prefix
+        entry: {
+            'vendor': [
+                'bootstrap/dist/css/bootstrap.css',
+                'event-source-polyfill',
+                'history',
+                'react',
+                'react-dom',
+                'react-router-dom',
+                'react-redux',
+                'redux',
+                'redux-thunk',
+                'react-router-redux',
+                'jquery'
+            ],
+            'main-client': './ClientApp/boot-client.tsx',
         },
         module: {
             rules: [
+                { test: /\.css$/, use: [ MiniCssExtractPlugin.loader, isDevBuild ? 'css-loader' : 'css-loader?minimize' ]},
                 { test: /\.tsx?$/, include: /ClientApp/, use: [{ loader: 'ts-loader', options: { transpileOnly: true }}] },
                 { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' },
                 { test: /\.ttf$/, use: 'url-loader?limit=25000&name=[hash].[ext]' }
             ]
         },
-        // plugins: [new CheckerPlugin()]
-    });
+        output: {
+            filename: '[name].js',
+            path: path.join(__dirname, clientBundleOutputDir) },
 
-    // Configuration for client-side bundle suitable for running in browsers
-    const clientBundleOutputDir = './public/dist';
-    const clientBundleConfig = merge(sharedConfig(), {
-        entry: { 'main-client': './ClientApp/boot-client.tsx' },
-        module: {
-            rules: [
-                {
-                    test: /\.css$/,
-                    use: [ MiniCssExtractPlugin.loader, isDevBuild ? 'css-loader' : 'css-loader?minimize' ]
-                }
-            ]
-        },
-        output: { path: path.join(__dirname, clientBundleOutputDir) },
         plugins: [
             new MiniCssExtractPlugin(),
-            // new webpack.DllReferencePlugin({
-            //     context: __dirname,
-            //     manifest: require('./wwwroot/dist/vendor-manifest.json')
-            // })
             new HtmlWebpackPlugin({
                 template: "public/index.html",
+            }),
+            new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }), // Maps these identifiers to the jQuery package (because Bootstrap expects it to be a global variable)
+            new webpack.NormalModuleReplacementPlugin(/\/iconv-loader$/, require.resolve('node-noop')), // Workaround for https://github.com/andris9/encoding/issues/16
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': isDevBuild ? '"development"' : '"production"'
             }),
         ].concat(isDevBuild ? [
             // Plugins that apply in development builds only
             new webpack.SourceMapDevToolPlugin({
                 filename: '[file].map', // Remove this line if you prefer inline source maps
-                moduleFilenameTemplate: path.relative(clientBundleOutputDir, '[resourcePath]') // Point sourcemap entries to the original file locations on disk
+                moduleFilenameTemplate: path.relative(clientBundleOutputDir, '[resourcePath]'), // Point sourcemap entries to the original file locations on disk
+                exclude: ['vendor.js']
             })
         ] : [
             // Plugins that apply in production builds only
@@ -63,28 +63,10 @@ module.exports = (env) => {
                     workers: 2
                 }
             })
-        ])
-    });
+        ]),
+        stats: { modules: false },
+        resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    };
 
-    // Configuration for server-side (prerendering) bundle suitable for running in Node
-    // const serverBundleConfig = merge(sharedConfig(), {
-    //     resolve: { mainFields: ['main'] },
-    //     entry: { 'main-server': './ClientApp/boot-server.tsx' },
-    //     plugins: [
-    //         new webpack.DllReferencePlugin({
-    //             context: __dirname,
-    //             manifest: require('./ClientApp/dist/vendor-manifest.json'),
-    //             sourceType: 'commonjs2',
-    //             name: './vendor'
-    //         })
-    //     ],
-    //     output: {
-    //         libraryTarget: 'commonjs',
-    //         path: path.join(__dirname, './ClientApp/dist')
-    //     },
-    //     target: 'node',
-    //     devtool: 'inline-source-map'
-    // });
-
-    return [clientBundleConfig ];//, serverBundleConfig];
+    return [clientBundleConfig];
 };
